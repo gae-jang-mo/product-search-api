39d271cc60ac2fa0726836b3f93d85dfb9997338
JunHoPark93 <pjh374968@gmail.com>
2020-05-31 14:06:18 +0900

    naver property yml AWS property 설정 (#12)

--- appspec/scripts/deploy.sh
+++ appspec/scripts/deploy.sh
@@ -6,7 +6,7 @@ PROJECT_NAME=product-search-api
 cd $REPOSITORY
 
 echo "> build 파일 복사"
-cp $REPOSITORY/zip/*.jar $REPOSITORY/$PROJECT_NAME
+cp $REPOSITORY/zip/*.jar $REPOSITORY/$PROJECT_NAME.jar
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl $PROJECT_NAME)
@@ -27,7 +27,7 @@ echo ">$JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> 개장모 메인 서버 배포 합니다 !!"
-nohup java -jar -Dspring.profiles.active=dev /home/ec2-user/product-search-api/deploy/zip/product-search-api-0.0.1-SNAPSHOT.jar >> /home/ec2-user/product-search-api/logs/api-server.log &
+nohup java -jar -Dspring.profiles.active=dev /home/ec2-user/product-search-api/deploy/zip/product-search-api-0.0.1-SNAPSHOT.jar >> /home/ec2-user/product-search-api/logs/product-search-api.log &
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/ |grep *.jar | tail -n 1)
